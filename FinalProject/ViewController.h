6d7182d999665392a060fca8384f757dfdd0fe3c
Brent Dady <brentdady@gmail.com>
2015-06-12 19:46:54 -0700

    Initial Commit

---
+++ FinalProject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FinalProject
+//
+//  Created by Brent Dady on 6/12/15.
+//  Copyright (c) 2015 Brent Dady. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ FinalProject/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  FinalProject
+//
+//  Created by Brent Dady on 6/12/15.
+//  Copyright (c) 2015 Brent Dady. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
