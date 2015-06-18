5e9f218adb714c61be0cffae5e1236167f90c6a9
Brent Dady <brentdady@gmail.com>
2015-06-18 16:28:22 -0700

    dropped in settings view & segue to reminders view

---
+++ FinalProject/SettingsViewController.h
@@ -0,0 +1,13 @@
+//
+//  SettingsViewController.h
+//  FinalProject
+//
+//  Created by Brent Dady on 6/18/15.
+//  Copyright (c) 2015 Brent Dady. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SettingsViewController : UIViewController
+
+@end
