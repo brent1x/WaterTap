b196a8af82d05f769146e1be6f90d9cca2a1f5e6
NNeyzi <naderneyzi@gmail.com>
2015-06-18 10:39:42 -0700

    add missing cocoapods files

---
+++ Pods/Target Support Files/Pods/Pods-environment.h
@@ -0,0 +1,20 @@
+
+// To check if a library is compiled with CocoaPods you
+// can use the `COCOAPODS` macro definition which is
+// defined in the xcconfigs so it is available in
+// headers also when they are imported in the client
+// project.
+
+
+// BEMSimpleLineGraph
+#define COCOAPODS_POD_AVAILABLE_BEMSimpleLineGraph
+#define COCOAPODS_VERSION_MAJOR_BEMSimpleLineGraph 4
+#define COCOAPODS_VERSION_MINOR_BEMSimpleLineGraph 0
+#define COCOAPODS_VERSION_PATCH_BEMSimpleLineGraph 0
+
+// PDTSimpleCalendar
+#define COCOAPODS_POD_AVAILABLE_PDTSimpleCalendar
+#define COCOAPODS_VERSION_MAJOR_PDTSimpleCalendar 0
+#define COCOAPODS_VERSION_MINOR_PDTSimpleCalendar 7
+#define COCOAPODS_VERSION_PATCH_PDTSimpleCalendar 1
+
