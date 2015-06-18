c3b04b9f766fa72f95646c8e35d0bfab52180bc4
Brent Dady <brentdady@gmail.com>
2015-06-18 10:14:02 -0700

    stubbed in rootVC

--- FinalProject/RootViewController.h
+++ FinalProject/RootViewController.h
@@ -9,7 +9,7 @@
 #import <UIKit/UIKit.h>
 @import HealthKit;
 
-@interface ViewController : UIViewController
+@interface RootViewController : UIViewController
 
 @property (nonatomic) HKHealthStore *healthStore;
 
---
+++ FinalProject/RootViewController.m
@@ -0,0 +1,22 @@
+//
+//  ViewController.m
+//  FinalProject
+//
+//  Created by Brent Dady on 6/12/15.
+//  Copyright (c) 2015 Brent Dady. All rights reserved.
+//
+
+#import "RootViewController.h"
+
+@interface RootViewController ()
+
+@end
+
+@implementation RootViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+
+}
+
+@end
