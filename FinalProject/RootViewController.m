4083956bb93363d2810ef8f79e0af55b64eafac7
Anders Chaplin <achaplin17@cmc.edu>
2015-06-29 20:43:45 -0700

    WCLvlFunctioning

--- FinalProject/RootViewController.m
+++ FinalProject/RootViewController.m
@@ -86,6 +86,8 @@
 
     [self.navigationController setNavigationBarHidden:YES animated:YES];
 
+    [self.navigationController setNavigationBarHidden:YES animated:YES];
+
     [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(dateCheck) name:UIApplicationDidBecomeActiveNotification object:nil];
     [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(dateCheck) name:UIApplicationWillEnterForegroundNotification object:nil];
 
